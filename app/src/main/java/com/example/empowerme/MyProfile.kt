614991012fadf413e73c9e0d6d2041c754abe9c7
Dinithi Mendis <dinithisamindi27@gmail.com>
2023-10-02 22:37:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/empowerme/MyProfile.kt
@@ -0,0 +1,31 @@
+package com.example.empowerme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MyProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+
+        val jobButton = findViewById<ImageButton>(R.id.imageButton6)
+        jobButton.setOnClickListener {
+            val Intent = Intent(this, JobPostActivity::class.java)
+            startActivity(Intent)
+        }
+
+        val appButton = findViewById<ImageButton>(R.id.imageButton7)
+        appButton.setOnClickListener {
+            val Intent = Intent(this, ApplicantsActivity::class.java)
+            startActivity(Intent)
+        }
+
+        val bButton = findViewById<ImageButton>(R.id.button31)
+        bButton.setOnClickListener {
+            val Intent = Intent(this, StartUpActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+}
